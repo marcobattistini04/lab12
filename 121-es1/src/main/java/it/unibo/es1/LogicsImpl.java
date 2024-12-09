779c21664cd80d4b8740dcb1ddd053e9fcfcfacf
Marco Battistini <marco.battistini10@studio.unibo.it>
2024-12-09 10:02:07 +0100

    first implementation of LogicsImpl.java in folder 121

--- 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
+++ 121-es1/src/main/java/it/unibo/es1/LogicsImpl.java
@@ -1,46 +1,73 @@
 package it.unibo.es1;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LogicsImpl implements Logics {
+	private final int size;
+	private List<Integer> values = new ArrayList<>();
 
 	public LogicsImpl(int size) {
-		//TODO Auto-generated constructor stub
+		this.size = size;
+		for (int i = 0; i < size; i++){
+			this.values.addLast(0);
+		}
 	}
 
 	@Override
 	public int size() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'size'");
+		return this.size;
 	}
 
 	@Override
 	public List<Integer> values() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'values'");
+		return List.copyOf(this.values);
 	}
 
 	@Override
 	public List<Boolean> enablings() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'enablings'");
+		List<Boolean> enabledButtons = new ArrayList<>();
+		for (int elem : this.values) {
+			if(elem == this.size) {
+				enabledButtons.addLast(false);
+			} else {
+				enabledButtons.addLast(true);
+			}
+		}
+		return enabledButtons;
 	}
 
 	@Override
 	public int hit(int elem) {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'hit'");
+		this.values.set(elem, this.values.get(elem) + 1);
+		return this.values.get(elem);
 	}
 
+	private boolean sameValues() {
+		final int currentValue = this.values.get(0);
+		for (final int elem : this.values) {
+			if(currentValue != elem) {
+				return false;
+			}
+		}
+		return true;
+	}
 	@Override
 	public String result() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'result'");
+		String result = "";
+		result = result + "<<";
+		for (int i = 0; i < this.size -1 ; i++) {
+			result = result + this.values.get(i) + "|";
+		}
+		result = result + this.values.get(size - 1) + ">>";
+		return result;
 	}
 
 	@Override
 	public boolean toQuit() {
-		// TODO Auto-generated method stub
-		throw new UnsupportedOperationException("Unimplemented method 'toQuit'");
+		if (sameValues()) {
+			return true;
+		}
+		return false;
 	}
 }
